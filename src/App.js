ef31642d3fdcbb1c10b453cdf4c50a7bc4966d1f
GuilhermeGomes76 <guilhermegomessilva76@gmail.com>
2025-09-15 21:10:06 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logoWhats from './images/—Pngtree—whatsapp phone icon_8704826.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div class="whats">
+        <a href="https://wa.me/5511966884083">
+            <img src={logoWhats} alt="" height="80px"/>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
