4aadf4d27ac9d75086ba67d8082415427e4c1fe7
GuilhermeGomes76 <guilhermegomessilva76@gmail.com>
2025-09-17 21:44:34 -0300

    esboço landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
-import logoWhats from './images/—Pngtree—whatsapp phone icon_8704826.png'
+import logoSpTrans from './images/spTrans.png';
 
 function App() {
   return (
     <div className="App">
-      <div class="whats">
-        <a href="https://wa.me/5511966884083">
-            <img src={logoWhats} alt="" height="80px"/>
-        </a>
-      </div>
+      {/* Header */}
+      <header>
+        <div className="header-container">
+          {/* Logo (leva para SPTrans) */}
+          <a 
+            href="https://lv.sbe.sptrans.com.br/vcw/login.action" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="logo"
+          >
+            <img src={logoSpTrans} alt="SPTrans" />
+            <span>SPTrans</span>
+          </a>
+
+          {/* Menu */}
+          <nav className="nav-menu">
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#sobre">Sobre</a></li>
+              <li><a href="#contato">Contato</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      {/* Botão SPTrans fixo no canto inferior esquerdo */}
+      <a 
+        href="https://lv.sbe.sptrans.com.br/vcw/login.action" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="btn-fixed"
+      >
+        Recarga
+      </a>
     </div>
   );
 }
